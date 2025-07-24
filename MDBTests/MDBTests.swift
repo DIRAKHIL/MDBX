e2356b88bd750e5853d4b478b776404d45885953
dirakhil <dir.akhilmaddali@gmail.com>
2025-07-25 02:03:35 +0530

    Initial Commit

---
+++ MDBTests/MDBTests.swift
@@ -0,0 +1,17 @@
+//
+//  MDBTests.swift
+//  MDBTests
+//
+//  Created by Akhil Maddali on 25/07/25.
+//
+
+import Testing
+@testable import MDB
+
+struct MDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
